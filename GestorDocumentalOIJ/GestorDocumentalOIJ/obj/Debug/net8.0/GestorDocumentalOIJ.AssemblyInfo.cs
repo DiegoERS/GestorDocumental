e19bb39d86670f91907a8e88e606779623f3a860
DiegoERS <diegoerios10@gmail.com>
2024-11-02 08:18:26 -0600

    Revert "comentado x2"
    This reverts commit 3cfb35bc25fab93d611464c2d83d5b1384d7e458.

--- GestorDocumentalOIJ/GestorDocumentalOIJ/obj/Debug/net8.0/GestorDocumentalOIJ.AssemblyInfo.cs
+++ GestorDocumentalOIJ/GestorDocumentalOIJ/obj/Debug/net8.0/GestorDocumentalOIJ.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestorDocumentalOIJ")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+343eca04497ea87e75f96f36db8eb32d4456901b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d55eb50206c8d6e851cbef55e97a4b73a22534d")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestorDocumentalOIJ")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestorDocumentalOIJ")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
