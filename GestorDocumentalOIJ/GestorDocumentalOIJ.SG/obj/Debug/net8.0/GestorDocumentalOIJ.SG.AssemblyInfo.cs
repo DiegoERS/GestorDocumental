3d7803a3fba25312d57fae9ae63cccdac9053c21
DiegoERS <diegoerios10@gmail.com>
2024-11-12 22:53:26 -0600

    arreglo de clase duplicada de reportes

--- GestorDocumentalOIJ/GestorDocumentalOIJ.SG/obj/Debug/net8.0/GestorDocumentalOIJ.SG.AssemblyInfo.cs
+++ GestorDocumentalOIJ/GestorDocumentalOIJ.SG/obj/Debug/net8.0/GestorDocumentalOIJ.SG.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("GestorDocumentalOIJ.SG")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b3b8558fee4895afc4d66ad70c2223584b1ed41d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2e43fdc60e3a677a3f0be8aaf275b70f1b724ad7")]
 [assembly: System.Reflection.AssemblyProductAttribute("GestorDocumentalOIJ.SG")]
 [assembly: System.Reflection.AssemblyTitleAttribute("GestorDocumentalOIJ.SG")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
